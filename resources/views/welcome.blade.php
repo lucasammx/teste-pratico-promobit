5effbf88a61fef5f37f32227c25fe91b14da209f
lucas <lucasaeciomm@gmail.com>
2022-02-22 23:01:08 -0300

    Correçcao final frontend ç

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,25 +9,25 @@
     <title>frontenddesafio</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@latest/css/materialdesignicons.min.css">
+    <link href="/css/chunk-2dbbc46b.ddf39ab9.css" rel="prefetch">
     <link href="/css/chunk-459f9190.51f6f92e.css" rel="prefetch">
     <link href="/css/chunk-4fc49947.920d480f.css" rel="prefetch">
     <link href="/css/chunk-58b222d7.998f4302.css" rel="prefetch">
-    <link href="/css/chunk-60716402.0879d462.css" rel="prefetch">
     <link href="/css/chunk-62493d32.ecb7680e.css" rel="prefetch">
     <link href="/css/chunk-709127f6.ac9209f1.css" rel="prefetch">
     <link href="/css/chunk-98f11088.f4598581.css" rel="prefetch">
     <link href="/css/chunk-ba51bb92.fbc4c5c1.css" rel="prefetch">
+    <link href="/js/chunk-2dbbc46b.f695e890.js" rel="prefetch">
     <link href="/js/chunk-459f9190.5f18a244.js" rel="prefetch">
     <link href="/js/chunk-4fc49947.d1e8e006.js" rel="prefetch">
     <link href="/js/chunk-58b222d7.19940bd4.js" rel="prefetch">
-    <link href="/js/chunk-60716402.3dc5af15.js" rel="prefetch">
-    <link href="/js/chunk-62493d32.83666063.js" rel="prefetch">
+    <link href="/js/chunk-62493d32.b9d6c506.js" rel="prefetch">
     <link href="/js/chunk-709127f6.0dc1cf58.js" rel="prefetch">
-    <link href="/js/chunk-98f11088.7c28b4c1.js" rel="prefetch">
+    <link href="/js/chunk-98f11088.cc0cacfd.js" rel="prefetch">
     <link href="/js/chunk-ba51bb92.6e603410.js" rel="prefetch">
     <link href="/css/app.2d471ebf.css" rel="preload" as="style">
     <link href="/css/chunk-vendors.92525c3d.css" rel="preload" as="style">
-    <link href="/js/app.a9831094.js" rel="preload" as="script">
+    <link href="/js/app.65ad4c34.js" rel="preload" as="script">
     <link href="/js/chunk-vendors.11805de0.js" rel="preload" as="script">
     <link href="/css/chunk-vendors.92525c3d.css" rel="stylesheet">
     <link href="/css/app.2d471ebf.css" rel="stylesheet">
@@ -36,7 +36,7 @@
 <body><noscript><strong>We're sorry but frontenddesafio doesn't work properly without JavaScript enabled. Please enable it to continue.</strong></noscript>
     <div id="app"></div>
     <script src="/js/chunk-vendors.11805de0.js"></script>
-    <script src="/js/app.a9831094.js"></script>
+    <script src="/js/app.65ad4c34.js"></script>
 </body>
 
 </html>
